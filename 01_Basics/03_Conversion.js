f947c03434fb582ae60d509b269bf039542f9175
AdamSonja <146699451+AdamSonja@users.noreply.github.com>
2025-02-25 05:22:57 +0000

    ConversionOperation

---
+++ 01_Basics/03_Conversion.js
@@ -0,0 +1,14 @@
+// let score = "33";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+
+let isLoggedin ="Sunny";
+let booleanIsLoggedIn= Boolean(isLoggedin);
+console.log(booleanIsLoggedIn);
+SomeNumber =12;
+let StringNumber = String(SomeNumber);
+console.log(StringNumber);
