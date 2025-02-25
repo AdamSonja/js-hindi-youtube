095d3d3a904757e5e1eb3a9c5932e927c0bfc46d
AdamSonja <146699451+AdamSonja@users.noreply.github.com>
2025-02-25 04:56:39 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all version of code as newer version
+
+// alert("hello ")/* We are using nodeejs not browser so this wont run */
+console.log(3+3);console.log("Sunny");
+
+let name = "Sunny";
+let age = 21;
+let isloggedin = false;
+
+/*Data types in Js */
+/*
+number
+bigint
+string
+boolean
+null->standalone value
+undefined->empty value
+symbol
+Object
+
+
+*/
+//console.log(typeof BigInt);
