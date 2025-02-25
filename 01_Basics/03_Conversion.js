adeda13e699fc8c1f937d4b8e602d6712829e99e
AdamSonja <146699451+AdamSonja@users.noreply.github.com>
2025-02-25 10:28:49 +0000

    ConversionOperation

--- 01_Basics/03_Conversion.js
+++ 01_Basics/03_Conversion.js
@@ -6,9 +6,37 @@
 // let valueInNumber=Number(score);
 // console.log(typeof valueInNumber);
 
-let isLoggedin ="Sunny";
-let booleanIsLoggedIn= Boolean(isLoggedin);
-console.log(booleanIsLoggedIn);
-SomeNumber =12;
-let StringNumber = String(SomeNumber);
-console.log(StringNumber);
+// let isLoggedin ="Sunny";
+// let booleanIsLoggedIn= Boolean(isLoggedin);
+// console.log(booleanIsLoggedIn);
+// SomeNumber =12;
+// let StringNumber = String(SomeNumber);
+// console.log(StringNumber);
+
+//***************************Operations*********************************** */ */
+let value = 3;
+let negValue= -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2**3);
+// console.log(2%3);
+
+// let str1 = "hello";
+// let str2 = "Sunny";
+// let str3 = str1+str2;
+// console.log(str3)
+// console.log(+"");
+// console.log(+true);
+// let num1,num2,num3
+// num1=num2=num3=2+2;
+
+let gameCounter=100;
+console.log(gameCounter );
+gameCounter++;
+console.log(gameCounter );
+++gameCounter;
+console.log(gameCounter );
